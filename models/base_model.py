e461a8ab5b463c503e59501642f4b5215327e533
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-05-15 19:38:59 -0400

    Airbnb console

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+Module contain class `Basemodel` that 
+defines all common attributes/methods for other classess
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict = {}
+        dict.update(self.__dict__)
+        dict.update({'__class__': self.__class__.__name__})
+        dict['created_at'] = self.created_at.isoformat()
+        dict['updated_at'] = self.updated_at.isoformat()
+        return dict
