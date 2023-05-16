63a6850f1bb398e43bbd7023bf4411989a81d5b1
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-05-16 08:55:25 -0400

    Filestorage added

--- models/base_model.py
+++ models/base_model.py
@@ -6,24 +6,43 @@ defines all common attributes/methods for other classess
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """ Class that defines all common methods and attributes for project """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+
 
     def __str__(self):
+        """String representation of the class"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
+        """Update date and time"""
         self.updated_at = datetime.now()
+        storage.save()
+
 
     def to_dict(self):
+        """
+        Return a dictionary containing all keys/values of
+        __dict__ of the instance
+        """
         dict = {}
         dict.update(self.__dict__)
         dict.update({'__class__': self.__class__.__name__})
---
+++ models/engine/file_storage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for airbnb clone"""
+import json
+
+
+class FileStorage:
+    """This class manages storage of airbnb models in JSON format"""
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns a dictionary of models currently in storage"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Adds new object to storage"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serialize dictionary into a json file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            tmp_dict = {}
+            tmp_dict.update(FileStorage.__objects)
+            for key, value in tmp_dict.items():
+                tmp_dict[key] = value.to_dict()
+            json.dump(tmp_dict, f)
+
+    def reload(self):
+        """ Deserialize and create instance of object saved in json file"""
+        try:
+            from models.base_model import BaseModel
+            with open(FileStorage.__file_path, 'r') as f:
+                tmp_dict = json.load(f)
+                for key, value in tmp_dict.items():
+                    tmp_obj = eval(value['__class__'] + "(**value)")
+                    FileStorage.__objects[key] = tmp_obj
+        except FileNotFoundError:
+            pass
