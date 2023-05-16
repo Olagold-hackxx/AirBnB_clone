c0a3c0295ae45740f44f19408a5f9965e92915b1
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-05-16 09:23:10 -0400

    Console added

---
+++ console.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Command line console / interpreter for Airbnb clone project
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command intepreter"""
+
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        """Exit console"""
+        exit()
+
+    def help_quit(self):
+        """ Prints the help documentation for quit  """
+        print("Quit command to exit console\n")
+
+    def do_EOF(self, arg):
+        """ Handles EOF to exit program """
+        print()
+        return True
+
+    def help_EOF(self):
+        """ Prints the help documentation for EOF """
+        print("Exits the program without formatting\n")
+
+    def emptyline(self):
+        """ Overrides the emptyline method of CMD """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
